1b2ecb1044c5ad01ccf253c25c955aab1baed87c
YourLocal-hub <dafuqboomlegitboom@gmail.com>
2025-06-05 23:27:58 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-// Function to handle the document selection and redirect
-document.getElementById('document-dropdown').addEventListener('change', function() {
-    const selectedDocument = this.value; // Get the selected document value
-    if (selectedDocument) {
-        window.location.href = selectedDocument; // Redirect to the selected document
-    }
-});
